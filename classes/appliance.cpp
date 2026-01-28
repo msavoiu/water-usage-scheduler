542487903ba9eb343e583c5279928bfad45d04f9
Madeline Savoiu <savoiu@csu.fullerton.edu>
2026-01-28 14:37:05 -0800

    removed total_water_usage member

--- classes/appliance.cpp
+++ classes/appliance.cpp
@@ -4,14 +4,12 @@ Appliance::Appliance(
     const std::string& name,
     double cycle_time,
     double water_usage_per_minute,
-    double total_water_usage,
     bool interruptable,
     bool takes_greywater
 )
     : name_(name),
       cycle_time_(cycle_time),
       water_usage_per_minute_(water_usage_per_minute),
-      total_water_usage_(total_water_usage),
       interruptable_(interruptable),
       takes_greywater_(takes_greywater)
 {
@@ -24,7 +22,6 @@ Appliance::Appliance(
     : name_(other.name()),
     cycle_time_(other.cycleTime()),
     water_usage_per_minute_(other.waterUsagePerMinute()),
-    total_water_usage_(other.totalWaterUsage()),
     interruptable_(other.interruptable()),
     takes_greywater_(other.takesGreywater())
 {}
@@ -33,7 +30,6 @@ Appliance::Appliance(
 void Appliance::setName(const std::string& name) { name_ = name; }
 void Appliance::setCycleTime(int minutes) { cycle_time_ = minutes; }
 void Appliance::setWaterUsagePerMinute(double liters) { water_usage_per_minute_ = liters; }
-void Appliance::setTotalWaterUsage(double total_water_usage) { total_water_usage_ = total_water_usage; }
 void Appliance::setInterruptable(bool interruptable) { interruptable_ = interruptable; }
 void Appliance::setTakesGreywater(bool takes_greywater) { takes_greywater_ = takes_greywater; }
 void Appliance::setProducesGreywater(bool produces_greywater) { produces_greywater_ = produces_greywater; }
@@ -42,7 +38,6 @@ void Appliance::setProducesGreywater(bool produces_greywater) { produces_greywat
 std::string Appliance::name() const { return name_; }
 int Appliance::cycleTime() const { return cycle_time_; }
 double Appliance::waterUsagePerMinute() const { return water_usage_per_minute_; }
-double Appliance::totalWaterUsage() const { return total_water_usage_; };
 bool Appliance::interruptable() const { return interruptable_; }
 bool Appliance::takesGreywater() const { return takes_greywater_; }
 bool Appliance::producesGreywater() const { return produces_greywater_; }
@@ -52,7 +47,6 @@ GreywaterAppliance::GreywaterAppliance(
     const std::string& name,
     int cycle_time,
     double water_usage_per_minute,
-    double total_water_usage,
     bool interruptable,
     bool takes_greywater,
     double water_output_per_cycle
@@ -61,7 +55,6 @@ GreywaterAppliance::GreywaterAppliance(
         name,
         cycle_time,
         water_usage_per_minute,
-        total_water_usage,
         interruptable,
         takes_greywater
     ),
--- classes/appliance.hpp
+++ classes/appliance.hpp
@@ -9,7 +9,6 @@ class Appliance {
             const std::string& name,
             double cycle_time,
             double water_usage_per_minute,
-            double total_water_usage,
             bool interruptable,
             bool takes_greywater
         );
@@ -22,7 +21,6 @@ class Appliance {
         void setName(const std::string& name);
         void setCycleTime(int minutes);
         void setWaterUsagePerMinute(double water_usage_per_minute);
-        void setTotalWaterUsage(double total_water_usage);
         void setInterruptable(bool interruptable);
         void setTakesGreywater(bool takes_greywater);
         void setProducesGreywater(bool produces_greywater);
@@ -32,7 +30,6 @@ class Appliance {
         std::string name() const;
         int cycleTime() const;
         double waterUsagePerMinute() const;
-        double totalWaterUsage() const;
         bool interruptable() const;
         bool takesGreywater() const;
         bool producesGreywater() const;
@@ -47,7 +44,6 @@ class Appliance {
         std::string name_;
         int cycle_time_; // minutes
         double water_usage_per_minute_;
-        double total_water_usage_;
         bool interruptable_; // some appliances, like washing machine, should not be stopped and left to sit for extended periods of time
         bool takes_greywater_;
         bool produces_greywater_;
@@ -60,7 +56,6 @@ class GreywaterAppliance: public Appliance {
             const std::string& name,
             int cycle_time,
             double water_usage_per_minute,
-            double total_water_usage,
             bool interruptable,
             bool takes_greywater,
             double water_output_per_cycle
